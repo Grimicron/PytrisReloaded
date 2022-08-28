5f632b743dd47de3c322617bb84e314a6d089e72
Grimicron <111781736+Grimicron@users.noreply.github.com>
2022-08-28 18:34:15 +0000

    Moving, collision, todo list

--- main.py
+++ main.py
@@ -58,7 +58,8 @@ class drawing_manager:
             for j in range(10):
                 if pieces[i][j] != "":
                     drawing_manager.draw_box(
-                        vec(j, i).mult(BOX_SIZE).add(BOARD_POS),
+                        # Not sure about why I have to have the +1
+                        vec(j, i+1).mult(BOX_SIZE).add(BOARD_POS),
                         drawing_manager.interpret_color(pieces[i][j]))
 
     @staticmethod
@@ -86,7 +87,7 @@ BOX_SIZE = 15
 BOARD_POS = vec(150, 50)
 SCREEN_SIZE = vec(500, 500)
 SCREEN = pygame.display.set_mode([SCREEN_SIZE.x, SCREEN_SIZE.y])
-FPS = 2.0
+GRAVITY = 2.0
 DAS = 4.0
 # FAST_MULTIPLIER cannot be one
 FAST_MULTIPLIER = 2.0
@@ -108,7 +109,7 @@ L_PIECE = ([[".X..", ".X..", ".XX.", "...."], ["....", "..X.", "XXX.", "...."],
 PIECES = [O_PIECE, T_PIECE, I_PIECE, Z_PIECE, S_PIECE, J_PIECE, L_PIECE]
 
 ################################
-# PIECE CLASSES
+# GAME CLASSES
 ################################
 
 
@@ -124,7 +125,10 @@ class piece:
     def draw(self):
         drawing_manager.draw_piece(self.pos, self.states[self.current_state],
                                    self.color)
-
+        
+    # Just to make code cleaner later on
+    def get_state(self, shift=0):
+        return self.states[(self.current_state+shift)%4]
 
 class dead_pieces:
     data = []
@@ -143,10 +147,11 @@ class dead_pieces:
 
     @staticmethod
     def add_piece(pos, state, color):
+        pos.y -= 1
         for i in range(4):
             for j in range(4):
                 if state[i][j] == "X":
-                    dead_pieces.data[pos.x + j][pos.y + i] = color
+                    dead_pieces.data[pos.y + i][pos.x + j] = color
 
     @staticmethod
     def clear_line(line):
@@ -161,29 +166,79 @@ class dead_pieces:
     def clear_lines():
         for i in range(20):
             full = True
-            for j in range(10):
+            for j in range(10):      
                 if dead_pieces.data[i][j] == "":
                     full = False
                     break
             if full:
                 dead_pieces.clear_line(i)
 
+    @staticmethod
+    def colliding(pos, state):
+        for i in range(4):
+            if pos.y+i >= 20:
+                return
+            for j in range(4):
+                if pos.x+j >= 10:
+                    # Same thing here except we only break because we still need to check
+                    # the other heights
+                    break
+                if state[i][j] == "X":
+                    if dead_pieces.data[pos.y + i][pos.x + j] != '':
+                        return True
+        return False
 
 class game:
     player_piece = None
-    fps_ts = time.time() + 1 / FPS
+    fps_ts = 0
     das_ts = 0
 
+    @staticmethod
+    def can_move(pos, state):
+        for i in range(4):
+            for j in range(4):
+                if state[i][j] == "X":
+                    if pos.x+j < 0:
+                        print("left")
+                        return False
+                    if pos.x+j >= 10:
+                        print("right")
+                        return False
+                    if pos.y+i >= 20:
+                        print("floor")
+                        return False
+        if dead_pieces.colliding(pos, state):
+            return False
+        return True
+
+    @staticmethod
+    def try_rotate(clockwise):
+        if clockwise:
+            if not(game.can_move(game.player_piece.pos, game.player_piece.get_state(1))):
+                return
+            game.player_piece.current_state += 1
+            game.player_piece.current_state %= 4
+        else:
+            if not(game.can_move(game.player_piece.pos, game.player_piece.get_state(-1))):
+                return
+            game.player_piece.current_state -= 1
+            game.player_piece.current_state %= 4
+            
+    
     @staticmethod
     def player_input():
         keys = pygame.key.get_pressed()
         game.fast = keys[pygame.K_DOWN]
         if keys[pygame.K_LEFT] and (time.time() > game.das_ts):
-            game.player_piece.pos.x += -1
-            game.das_ts = time.time() + 1.0 / DAS
+            if game.can_move(game.player_piece.pos.add(vec(-1, 0))
+              ,game.player_piece.get_state()):
+                game.player_piece.pos.x += -1
+                game.das_ts = time.time() + 1.0 / DAS    
         elif keys[pygame.K_RIGHT] and (time.time() > game.das_ts):
-            game.player_piece.pos.x += 1
-            game.das_ts = time.time() + 1.0 / DAS
+            if game.can_move(game.player_piece.pos.add(vec(1, 0))
+              ,game.player_piece.get_state()):
+                game.player_piece.pos.x += 1
+                game.das_ts = time.time() + 1.0 / DAS
 
     @staticmethod
     def frame():
@@ -192,23 +247,39 @@ class game:
         dead_pieces.draw()
         game.player_input()
         if time.time() > (game.fps_ts - 
-                          (((FAST_MULTIPLIER-1) / (FAST_MULTIPLIER*FPS) if game.fast else 0))):
-            game.player_piece.pos.y += 1
-            game.fps_ts = time.time() + 1 / FPS
+                          (((FAST_MULTIPLIER-1) / (FAST_MULTIPLIER*GRAVITY) if game.fast else 0))):
+            if game.can_move(game.player_piece.pos.add(vec(0, 1))
+                              ,game.player_piece.get_state()):
+                game.player_piece.pos.y += 1
+            else:
+                dead_pieces.add_piece(game.player_piece.pos.add(vec(0, 1))
+                                     ,game.player_piece.get_state()
+                                     ,game.player_piece.color)
+                game.player_piece = piece(random.choice(PIECES))
+            game.fps_ts = time.time() + 1 / GRAVITY
 
     @staticmethod
     def start():
         pygame.init()
         game.player_piece = piece(random.choice(PIECES))
         dead_pieces.init()
+        game.fps_ts = time.time() + 1 / GRAVITY
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                # Rotate piece clockwise/counterclockwise
+                # done here because this is the only place we can handle
+                # events and we want the rotate piece to be
+                # a key down event so it only happens once per press
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        game.try_rotate(True)
+                    elif event.key == pygame.K_LCTRL:
+                        game.try_rotate(False)
             game.frame()
             pygame.display.flip()
         pygame.quit()
-
 
 game.start()
